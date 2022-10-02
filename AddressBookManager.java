16092b230ada2e56a4ef83ca190a959a5dee30a6
Saqlain <iamkhatik786@gmail.com>
2022-10-02 14:52:23 +0530

    no duplicate entry

---
+++ AddressBookManager.java
@@ -0,0 +1,11 @@
+package com.bl.addressbook.streamapi;
+
+public class AddressBookManager {
+
+	public static void main(String[] args) {
+		AddressBook entry = new AddressBook();
+	
+		System.out.println("Welcome to Address Book Program in Java!");
+		entry.manageAddressBookList();
+	}
+}
